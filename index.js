2c872a4c6eb8aef56b82cb48cf67ab90cf4ac76a
Sudarshan <sudarshandevkota22@gmail.com>
2022-07-30 21:16:14 +0545

    theme change implemented with transition animation

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const sectionLinks = document.getElementsByClassName("section-links")[0]
-console.log(sectionLinks)
-console.log(toggleButton)
+const themeToggle = document.getElementById("dark-light")
+
 toggleButton.addEventListener('click',()=>{
     sectionLinks.classList.toggle('active')
     console.log("changed")
+
+})
+themeToggle.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-theme')
+    if(document.body.classList.contains('dark-theme'))
+        themeToggle.src= "images/sun.png"
+    else
+        themeToggle.src = "images/moon.png"
 
 })
