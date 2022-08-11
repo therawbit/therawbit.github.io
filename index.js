fd24869316aad8ec31a6add061c1998a8053eaee
Sudarshan <sudarshandevkota22@gmail.com>
2022-08-11 23:40:18 +0545

    project mapped through javascript and UI colors changed

--- index.js
+++ index.js
@@ -16,4 +16,26 @@ themeToggle.addEventListener('click',()=>{
 
 })
 
+// map projects to dom 
+const projects = [{"name":"Peak Productive","imageUrl":"images/pp.svg","info":"A Simple Productivity Focused Android app written in Java. It has basic tools for productivity like ToDo/Task to better manage task based on priority, Flash Card to quickly recall anything important and Pomodoro Clock to perform any task effeciently. ","githubUrl":"https://github.com/Sudru/PeakProductive"},
+{"name":"iOrder","imageUrl":"https://raw.githubusercontent.com/Sudru/iOrder/main/images/iOrder.png","info":"Android application created as a project for KU Hackfest 2022. It can be implemented by restaurant to allow their customers to order their foods with their own device by scanning the QR code placed at the table.","githubUrl":"https://github.com/Sudru/iOrder"},]
+const cardContainer = document.getElementsByClassName("cards-container")[0]
+
+cardContainer.innerHTML = projects.map(project =>
+    `
+    <div class="card">
+                <div class="face1" data-aos="${project.aos}">
+                    <img src="${project.imageUrl}">
+                    <div class="project-name">${project.name}</div>
+                </div>
+                <div class="face2">
+                    <div class="project-info">${project.info}</div>
+                    <div class="project-links">
+                        <a target="_blank" href="${project.githubUrl}"><i class="fa fa-github fa-2x"></i></a>
+                    </div>
+                </div>
+            </div>
+    `
+    )
+
 
