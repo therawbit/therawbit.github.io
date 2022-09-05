c9b322ff728f124572a71702f61055e3bc8d05f9
Sudarshan <sudarshandevkota22@gmail.com>
2022-09-05 11:55:44 +0545

    pp svg changed to png social icon changed from 2x to 3x

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ themeToggle.addEventListener('click',()=>{
 // map projects to dom 
 const projects = [{"name":"iOrder","imageUrl":"https://raw.githubusercontent.com/Sudru/iOrder/main/images/iOrder.png","info":"Android application created as a project for KU Hackfest 2022. It can be implemented by restaurant to allow their customers to order their foods with their own device by scanning the QR code placed at the table.","githubUrl":"https://github.com/Sudru/iOrder"},
 {"name":"YT-MP3","imageUrl":"https://raw.githubusercontent.com/Sudru/YT-Mp3/main/ytmp3-nobg.png","info":"A command line utility built on Python to download youtube videos as mp3 file. It is capable of downloading either a Single video or the whole Playlist based on the link you provide.","githubUrl":"https://github.com/Sudru/YT-Mp3"},
-{"name":"Peak Productive","imageUrl":"images/pp.svg","info":"A Simple Productivity Focused Android app written in Java. It has basic tools for productivity like ToDo/Task to better manage task based on priority, Flash Card to quickly recall anything important and Pomodoro Clock to perform any task effeciently. ","githubUrl":"https://github.com/Sudru/PeakProductive"},
+{"name":"Peak Productive","imageUrl":"images/pp.png","info":"A Simple Productivity Focused Android app written in Java. It has basic tools for productivity like ToDo/Task to better manage task based on priority, Flash Card to quickly recall anything important and Pomodoro Clock to perform any task effeciently. ","githubUrl":"https://github.com/Sudru/PeakProductive"},
 ]
 const cardContainer = document.getElementsByClassName("cards-container")[0]
 
